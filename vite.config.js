41982d97e67a87d5eb0d668c5fc8d0686ffecc47
Olena-Koval <19111982e@gmail.com>
2025-03-06 13:41:45 +0100

    Subir archivos generados con Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/diseno/' : '/', // Base para producción en GitHub Pages y local en desarrollo
+  base: process.env.NODE_ENV === 'production' ? '/diseno/' : '/', // Asegúrate de tener esta configuración
 })
